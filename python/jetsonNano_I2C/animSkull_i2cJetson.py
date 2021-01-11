36347bb5dcd93dd1eaf771c2ce150db79a7ae8bd
twinlizzie <denisefrom2018@gmail.com>
2021-01-11 18:05:02 +1300

    Improved Jetson Nano python code for Animatronic Skull

--- python/jetsonNano_I2C/animSkull_i2cJetson.py
+++ python/jetsonNano_I2C/animSkull_i2cJetson.py
@@ -3,6 +3,7 @@
 from adafruit_servokit import ServoKit
 from time import sleep
 
+import time
 import board
 import busio
 # import time
@@ -19,133 +20,195 @@ kit = ServoKit(channels=16, i2c=i2c_bus1)
 
 # These are the adresses we setup in the Arduino Program
 nema17z_address = 0x20
-nema17y_address = 0x10
+nema17x_address = 0x10
 
 # Slots on the PCA9685 PWM Driver, and the corresponding servos
 
-sg90_1 = 0
-sg90_2 = 1
-sg90_3 = 2
-sg90_4 = 3
+servo_0_eyeR_y = 0
+servo_1_eyeL_y = 1
+servo_2_eyeR_x = 2
+servo_3_eyeL_x = 3
 
-sg90_1b = 4
-sg90_2b = 5
-sg90_3b = 6
-sg90_4b = 7
+servo_4_lidR_top = 4
+servo_5_lidL_top = 5
+servo_6_lidR_bot = 6
+servo_7_lidL_bot = 7
 
-sg90_brow1 = 8
-sg90_brow2 = 9
+servo_8_browR = 8
+servo_9_browL = 9
 
-sg90_lip = 10
-sg90_corner1 = 11
-sg90_corner2 = 12
+servo_10_lip = 10
+servo_11_cornerR = 11
+servo_12_cornerL = 12
 
-sg90_jaw1 = 13
-sg90_jaw2 = 14
+servo_13_jawR = 13
+servo_14_jawL = 14
 
 # Offsets of articulation points... for fine tuning
 
-offset_lid_top_right = 0
-offset_lid_top_left = 25
+offset_lid_top_right = 10
+offset_lid_top_left = -5
 
-offset_lid_bottom_right = -30
-offset_lid_bottom_left = 0
+offset_lid_bottom_right = -20
+offset_lid_bottom_left = 30
 
 offset_right_y = -10
-offset_right_x = 0
+offset_right_x = -10
 
-offset_left_y = 17
+offset_left_y = 15
 offset_left_x = -10
 
-offset_brow1 = 10
-offset_brow2 = 0
+offset_brow1 = -15
+offset_brow2 = 5
 
-offset_lip = -7
-offset_corner1 = 5
-offset_corner2 = -17
+offset_lip = -34
+offset_corner1 = 0
+offset_corner2 = -8
 
-offset_jaw = -10
+offset_jaw = 45
+offset_jaw2 = 0
+
+# previous servo positions for smooth motion
+
+previous_pos_browR = 90
+previous_pos_browL = 90
+
+previous_pos_lip = 90
+
+previous_pos_cornerR = 90
+previous_pos_cornerL = 90
+
+previous_pos_jawR = 90
+previous_pos_jawL = 90
 
 # Set status of message and value
 
 previous_status = "0"
 previous_message = ""
 
-#def writeNumber(value, message):
-#i2c_bus0.writeto(address, value, stop=False)
-#    return -1
-
-def initialize_servos():
-
-	kit.servo[sg90_1].angle = 90 + offset_right_y
-	kit.servo[sg90_2].angle = 90 + offset_left_y
-	
-	kit.servo[sg90_3].angle = 90 + offset_right_x
-	kit.servo[sg90_4].angle = 90 + offset_left_x
-	
-	kit.servo[sg90_1b].angle = 90 + offset_lid_top_right
-	kit.servo[sg90_2b].angle = 90 + offset_lid_top_left
-	kit.servo[sg90_3b].angle = 90 + offset_lid_bottom_right
-	kit.servo[sg90_4b].angle = 90 + offset_lid_bottom_left
-	
-	kit.servo[sg90_jaw].angle = 90 + offset_jaw
-	
-	kit.servo[sg90_corner1].angle = 90 + offset_corner1         
-	kit.servo[sg90_corner2].angle = 90 + offset_corner2
-	
-	kit.servo[sg90_brow1].angle = 90 + offset_brow1         
-	kit.servo[sg90_brow2].angle = 90 + offset_brow2
-	
-	kit.servo[sg90_lip].angle = 90 + offset_lip
+default_msg0 = ""
+default_msg1 = "Welcome to SylvieOS 2021! App: Animatronic Skull"
+default_msg2 = "WAITING FOR USER INPUT! (TRY Q,W,E,R,T,A,S,D,F)"
 
 def clear_terminal(status):
 	global previous_status
 	if status != previous_status:
 		os.system('clear')
-		print("WAITING FOR USER INPUT! (TRY Q,W,E,R,T,A,S,D,F)")
-		# i2c_bus0.writeto(nema17z_address, status, stop=False)        
-		# i2c_bus0.writeto(nema17y_address, status, stop=False)
+		print(default_msg1)
+		print(default_msg2)
 		previous_status = status
 
-def sys_message(message):
+def sys_message(msg1, msg2=default_msg0):
 	global previous_message
-	if message != previous_message:
+	if msg1 != previous_message:
 		os.system('clear')
-		print(message)
-		previous_message = message        
+		print(msg1)
+		print(msg2)
+		previous_message = msg1    
+
+def initialize_servos():
+
+	kit.servo[servo_0_eyeR_y].angle = 90 + offset_right_y
+	kit.servo[servo_1_eyeL_y].angle = 90 + offset_left_y
+	
+	kit.servo[servo_2_eyeR_x].angle = 90 + offset_right_x
+	kit.servo[servo_3_eyeL_x].angle = 90 + offset_left_x
+	
+	kit.servo[servo_4_lidR_top].angle = 90 + offset_lid_top_right
+	kit.servo[servo_5_lidL_top].angle = 90 + offset_lid_top_left
+	kit.servo[servo_6_lidR_bot].angle = 90 + offset_lid_bottom_right
+	kit.servo[servo_7_lidL_bot].angle = 90 + offset_lid_bottom_left
+
+	kit.servo[servo_8_browR].angle = 90 + offset_brow1         
+	kit.servo[servo_9_browL].angle = 90 + offset_brow2	
+
+	kit.servo[servo_10_lip].angle = 90 + offset_lip
+	kit.servo[servo_11_cornerR].angle = 90 + offset_corner1         
+	kit.servo[servo_12_cornerL].angle = 90 + offset_corner2
+	
+	kit.servo[servo_13_jawR].angle = 90 + offset_jaw
+	kit.servo[servo_14_jawL].angle = 90 + offset_jaw2
+
+def move_eye_servos(posR_y=None, posL_y=None, posR_x=None, posL_x=None):
+
+	if posR_y is not None:	
+		kit.servo[servo_0_eyeR_y].angle = posR_y + offset_right_y
+	if posL_y is not None:
+		kit.servo[servo_1_eyeL_y].angle = posL_y + offset_left_y
+	if posR_x is not None:
+		kit.servo[servo_2_eyeR_x].angle = posR_x + offset_right_x
+	if posL_x is not None:	
+		kit.servo[servo_3_eyeL_x].angle = posL_x + offset_left_x
+
+def move_eyelid_servos(posR_top=None, posL_top=None, posR_bot=None, posL_bot=None):
+
+	if posR_top is not None:	
+		kit.servo[servo_4_lidR_top].angle = posR_top + offset_lid_top_right
+	if posL_top is not None:
+		kit.servo[servo_5_lidL_top].angle = posL_top + offset_lid_top_left
+	if posR_bot is not None:
+		kit.servo[servo_6_lidR_bot].angle = posR_bot + offset_lid_bottom_right
+	if posL_bot is not None:	
+		kit.servo[servo_7_lidL_bot].angle = posL_bot + offset_lid_bottom_left	
+
+def move_face_servos(pos_browR=None, pos_browL=None, pos_lip=None, pos_cornerR=None, pos_cornerL=None, pos_jawR=None, pos_jawL=None):
+
+	global previous_pos_browR
+	global previous_pos_browL
+
+	global previous_pos_lip
+
+	global previous_pos_cornerR
+	global previous_pos_cornerL
+
+	global previous_pos_jawR
+	global previous_pos_jawL
+
+	if pos_browR is not None:
+		kit.servo[servo_8_browR].angle = pos_browR + offset_brow1
+		previous_pos_browR = pos_browR
+	if pos_browL is not None:	
+		kit.servo[servo_9_browL].angle = pos_browL + offset_brow2
+		previous_pos_browL = pos_browL
+	if pos_lip is not None:	
+		kit.servo[servo_10_lip].angle = pos_lip + offset_lip
+		previous_pos_lip = pos_lip
+	if pos_cornerR is not None:	
+		kit.servo[servo_11_cornerR].angle = pos_cornerR + offset_corner1
+		previous_pos_cornerR = pos_cornerR
+	if pos_cornerL is not None:	
+		kit.servo[servo_12_cornerL].angle = pos_cornerL + offset_corner2
+		previous_pos_cornerL = pos_cornerL
+	if pos_jawR is not None:	
+		kit.servo[servo_13_jawR].angle = pos_jawR + offset_jaw
+		previous_pos_jawR = pos_jawR
+	if pos_jawL is not None:	
+		kit.servo[servo_14_jawL].angle = pos_jawL + offset_jaw2
+		previous_pos_jawL = pos_jawL
 
 initialize_servos() 
 
 while True:
 	if keyboard.is_pressed('q'): 	
 		sys_message("KEYBOARD KEY [Q] PRESSED!")
-		
-		kit.servo[sg90_1b].angle = 5 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 155 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 135 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 35 + offset_lid_bottom_left
+
+		move_eyelid_servos(15, 155, 120, 50) # Fixed this after Promo video #1. Bottom eyelids were too high!
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('w'): 	
 		sys_message("KEYBOARD KEY [W] PRESSED!")
-		
-		kit.servo[sg90_1b].angle = 90 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 90 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 90 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 90 + offset_lid_bottom_left
+
+		move_eyelid_servos(90, 90, 90, 90) # Eyelashes make it easier to close the eyes completely.
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('e'): 	
-		sys_message("KEYBOARD KEY [E] PRESSED!")
-		
-		kit.servo[sg90_1b].angle = 65 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 115 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 115 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 65 + offset_lid_bottom_left        
+		sys_message("KEYBOARD KEY [E] PRESSED!")       
+
+		move_eyelid_servos(60, 120, 120, 60)
 		
 		sleep(0.25)  
 		clear_terminal(0)
@@ -153,74 +216,49 @@ while True:
 	elif keyboard.is_pressed('r'): 	
 		sys_message("KEYBOARD KEY [R] PRESSED!") 
 		
-		kit.servo[sg90_1b].angle = 90 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 90 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 90 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 90 + offset_lid_bottom_left
-		
+		move_eyelid_servos(90, 90, 90, 90)
+
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('a'): 	
 		sys_message("KEYBOARD KEY [A] PRESSED!") 
-		
-		kit.servo[sg90_1].angle = 100 + offset_right_y
-		kit.servo[sg90_2].angle = 65 + offset_left_y
-		
-		kit.servo[sg90_1b].angle = 100 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 80 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 100 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 80 + offset_lid_bottom_left
-		
+
+		move_eye_servos(105, 70)		
+		move_eyelid_servos(120, 60, 95, 85)
+
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('s'): 	
 		sys_message("KEYBOARD KEY [S] PRESSED!")
-		
-		kit.servo[sg90_1].angle = 75 + offset_right_y
-		kit.servo[sg90_2].angle = 130 + offset_left_y
-		
-		kit.servo[sg90_1b].angle = 70 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 110 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 70 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 110 + offset_lid_bottom_left
+
+		move_eye_servos(75, 100)		
+		move_eyelid_servos(80, 100, 75, 115)
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('d'): 	
 		sys_message("KEYBOARD KEY [D] PRESSED!")
-		
-		kit.servo[sg90_3].angle = 65 + offset_right_x
-		kit.servo[sg90_4].angle = 65 + offset_left_x
+
+		move_eye_servos(None, None, 65, 120)
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('f'): 	
 		sys_message("KEYBOARD KEY [F] PRESSED!")
-		
-		kit.servo[sg90_3].angle = 110 + offset_right_x
-		kit.servo[sg90_4].angle = 120 + offset_left_x
+
+		move_eye_servos(None, None, 130, 60)
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
-	# elif keyboard.is_pressed('z'): 	
-	#    sys_message("KEYBOARD KEY [Z] PRESSED!")
-	#    sleep(0.25)
-	#    clear_terminal(0)
-	
-	# elif keyboard.is_pressed('x'): 	
-	#    sys_message("KEYBOARD KEY [X] PRESSED!")         
-	#    sleep(0.25)
-	#    clear_terminal(0)
-	
 	elif keyboard.is_pressed('t'): 	
 		sys_message("KEYBOARD KEY [T] PRESSED!")   
 		
-		i2c_bus0.writeto(nema17z_address, "2", stop=False)
+		i2c_bus0.writeto(nema17x_address, "6", stop=False)
 		
 		sleep(0.25)
 		clear_terminal(0)
@@ -228,7 +266,7 @@ while True:
 	elif keyboard.is_pressed('y'): 	
 		sys_message("KEYBOARD KEY [Y] PRESSED!") 
 		
-		i2c_bus0.writeto(nema17z_address, "3", stop=False)
+		i2c_bus0.writeto(nema17x_address, "7", stop=False)
 		
 		sleep(0.25)
 		clear_terminal(0)
@@ -236,7 +274,7 @@ while True:
 	elif keyboard.is_pressed('g'): 	
 		sys_message("KEYBOARD KEY [G] PRESSED!")    
 		
-		i2c_bus0.writeto(nema17y_address, "2", stop=False)
+		i2c_bus0.writeto(nema17z_address, "2", stop=False)
 		
 		sleep(0.25)
 		clear_terminal(0)
@@ -244,7 +282,7 @@ while True:
 	elif keyboard.is_pressed('h'): 	
 		sys_message("KEYBOARD KEY [H] PRESSED!")
 		
-		i2c_bus0.writeto(nema17y_address, "3", stop=False)
+		i2c_bus0.writeto(nema17z_address, "3", stop=False)
 		
 		sleep(0.25)
 		clear_terminal(0)
@@ -252,33 +290,31 @@ while True:
 	elif keyboard.is_pressed('b'): 	
 		sys_message("KEYBOARD KEY [B] PRESSED!")         
 		
-		kit.servo[sg90_brow1].angle = 50 + offset_brow1
-		kit.servo[sg90_brow2].angle = 130 + offset_brow2
+		move_face_servos(80, 100)
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('n'): 	
 		sys_message("KEYBOARD KEY [N] PRESSED!")  
-		
-		kit.servo[sg90_brow1].angle = 110 + offset_brow1
-		kit.servo[sg90_brow2].angle = 70 + offset_brow2
+
+		move_face_servos(100, 80)
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('u'): 	
 		sys_message("KEYBOARD KEY [U] PRESSED!")
-		
-		kit.servo[sg90_lip].angle = 150 + offset_lip
+
+		move_face_servos(None, None, 110)
 		
 		sleep(0.25)
 		clear_terminal(0)
 	
 	elif keyboard.is_pressed('i'): 	
 		sys_message("KEYBOARD KEY [I] PRESSED!")     
-		
-		kit.servo[sg90_lip].angle = 90 + offset_lip
+
+		move_face_servos(None, None, 90)
 		
 		sleep(0.25)
 		clear_terminal(0)   
@@ -286,97 +322,58 @@ while True:
 	elif keyboard.is_pressed('j'): 	
 		sys_message("KEYBOARD KEY [J] PRESSED!")
 		
-		kit.servo[sg90_corner1].angle = 70 + offset_corner1
-		kit.servo[sg90_corner2].angle = 110 + offset_corner2         
-		
-		kit.servo[sg90_1b].angle = 90 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 90 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 90 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 90 + offset_lid_bottom_left
-		
-		kit.servo[sg90_brow1].angle = 50 + offset_brow1
-		kit.servo[sg90_brow2].angle = 130 + offset_brow2
-		
-		kit.servo[sg90_lip].angle = 90 + offset_lip
-		
+		move_face_servos(90, 90, 90, 90, 90, 90, 90)
+		time.sleep(0.05)
+		move_face_servos(80, 100, 90, 80, 100, 90, 90)
+		time.sleep(0.05)
+		move_eyelid_servos(90, 90, 90, 90)
+
 		sleep(0.25)
 		clear_terminal(0) 
 	
 	elif keyboard.is_pressed('k'): 	
 		sys_message("KEYBOARD KEY [K] PRESSED!")   
-		
-		kit.servo[sg90_corner1].angle = 120 + offset_corner1
-		kit.servo[sg90_corner2].angle = 60 + offset_corner2         
-		
-		kit.servo[sg90_1b].angle = 70 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 110 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 110 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 70 + offset_lid_bottom_left
-		
-		kit.servo[sg90_brow1].angle = 85 + offset_brow1
-		kit.servo[sg90_brow2].angle = 85 + offset_brow2
-		
-		kit.servo[sg90_lip].angle = 105 + offset_lip
+
+		move_face_servos(90, 90, 90, 90, 90, 90, 90)
+		time.sleep(0.05)
+		move_face_servos(95, 85, 95, 100, 80, 115, 65)
+		time.sleep(0.05)
+		move_eyelid_servos(70, 110, 110, 70)
 		
 		sleep(0.25)
 		clear_terminal(0) 
 	
 	elif keyboard.is_pressed('1'): 	
 		sys_message("KEYBOARD KEY [1] PRESSED!")         
-		
-		kit.servo[sg90_jaw].angle = 160 + offset_jaw
-		
-		kit.servo[sg90_corner1].angle = 60 + offset_corner1         
-		kit.servo[sg90_corner2].angle = 120 + offset_corner2
-		
-		kit.servo[sg90_brow1].angle = 100 + offset_brow1         
-		kit.servo[sg90_brow2].angle = 80 + offset_brow2
-		
-		kit.servo[sg90_lip].angle = 90 + offset_lip
-		
+
+		move_face_servos(105, 80, 100, 80, 100, 110, 70)
+		time.sleep(0.05)
+		move_face_servos(105, 80, 100, 80, 100, 120, 60)
+		time.sleep(0.05)		
+		move_face_servos(105, 80, 100, 80, 100, 130, 50)
+
 		sleep(0.25)
 		clear_terminal(0)  
 	
 	elif keyboard.is_pressed('2'): 	
 		sys_message("KEYBOARD KEY [2] PRESSED!")         
-		
-		kit.servo[sg90_jaw].angle = 70 + offset_jaw
-		
-		kit.servo[sg90_corner1].angle = 90 + offset_corner1         
-		kit.servo[sg90_corner2].angle = 90 + offset_corner2
-		
-		kit.servo[sg90_brow1].angle = 90 + offset_brow1         
-		kit.servo[sg90_brow2].angle = 90 + offset_brow2
-		
-		kit.servo[sg90_lip].angle = 90 + offset_lip
-		
+
+		move_face_servos(90, 90, 90, 90, 90, 125, 55)
+		time.sleep(0.05)		
+		move_face_servos(90, 90, 90, 90, 90, 115, 65)
+		time.sleep(0.05)
+		move_face_servos(90, 90, 90, 90, 90, 105, 75)
+
 		sleep(0.25)
 		clear_terminal(0) 
 	
 	elif keyboard.is_pressed('o'): 	
 		sys_message("POSITIONS RESET!")
-		
-		kit.servo[sg90_1].angle = 90 + offset_right_y
-		kit.servo[sg90_2].angle = 90 + offset_left_y
-		
-		kit.servo[sg90_3].angle = 90 + offset_right_x
-		kit.servo[sg90_4].angle = 90 + offset_left_x
-		
-		kit.servo[sg90_1b].angle = 90 + offset_lid_top_right
-		kit.servo[sg90_2b].angle = 90 + offset_lid_top_left
-		kit.servo[sg90_3b].angle = 90 + offset_lid_bottom_right
-		kit.servo[sg90_4b].angle = 90 + offset_lid_bottom_left
-		
-		kit.servo[sg90_jaw].angle = 90 + offset_jaw
-		
-		kit.servo[sg90_corner1].angle = 90 + offset_corner1         
-		kit.servo[sg90_corner2].angle = 90 + offset_corner2
-		
-		kit.servo[sg90_brow1].angle = 90 + offset_brow1         
-		kit.servo[sg90_brow2].angle = 90 + offset_brow2
-		
-		kit.servo[sg90_lip].angle = 90 + offset_lip
-		
+
+		move_eye_servos(90, 90, 90, 90)
+		move_eyelid_servos(90, 90, 90, 90)
+		move_face_servos(90, 90, 90, 90, 90, 90, 90)	
+
 		sleep(0.25)  
 		clear_terminal(0)  
 	
@@ -387,7 +384,7 @@ while True:
 		os.system('clear')
 		exit()
 	else:		
-		sys_message("WAITING FOR USER INPUT! (TRY Q,W,E,R,T,A,S,D,F)")  
+		sys_message(default_msg1, default_msg2)  
 
 # while True:
 #    var = input("")
