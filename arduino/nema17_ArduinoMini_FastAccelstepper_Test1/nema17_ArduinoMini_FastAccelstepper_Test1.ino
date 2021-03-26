6d35d723cc1e00de9e610eda1bdfb13d858073c5
twinlizzie <denisefrom2018@gmail.com>
2021-03-26 13:08:51 +1300

    Added new Arduino and Python tester files.

--- arduino/nema17_ArduinoMini_FastAccelstepper_Test1/nema17_ArduinoMini_FastAccelstepper_Test1.ino
+++ arduino/nema17_ArduinoMini_FastAccelstepper_Test1/nema17_ArduinoMini_FastAccelstepper_Test1.ino
@@ -4,6 +4,8 @@
 #define enablePinStepper 4
 #define stepPinStepper   9  // OC1A in case of AVR
 
+int stepperState = 0;
+
 FastAccelStepperEngine engine = FastAccelStepperEngine();
 FastAccelStepper *stepper = NULL;
 
@@ -15,8 +17,8 @@ void setup() {
     stepper->setEnablePin(enablePinStepper);
     stepper->setAutoEnable(true);
 
-    stepper->setSpeed(20);       // the parameter is us/step !!!
-    stepper->setAcceleration(75000);
+    stepper->setSpeed(15);       // the parameter is us/step !!!
+    stepper->setAcceleration(45000);
   }
 }
 
