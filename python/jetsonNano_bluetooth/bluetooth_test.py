994a26e13379caecc910f1916b26a7772fff6cdf
twinlizzie <denisefrom2018@gmail.com>
2021-03-26 23:23:40 +1300

    Playing with Bluetooth

--- python/jetsonNano_bluetooth/bluetooth_test.py
+++ python/jetsonNano_bluetooth/bluetooth_test.py
@@ -9,7 +9,7 @@ client_socket,address = server_socket.accept()
 print("Accepted connection from ",address)
 
 while True:
-    res = self.client_socket.recv(1024)
+    res = client_socket.recv(1024)
     client_socket.send(res)
     if res == 'q':
         print ("Quit")
@@ -19,3 +19,16 @@ while True:
 
 client_socket.close()
 server_socket.close()
+
+# Commands used:
+# 1. sudo sdptool add SP
+# 2. rfcomm watch hci0
+# 3. minicom -b 9600 -o -D /dev/rfcomm0
+# 4. sudo nano /etc/systemd/system/dbus-org.bluez.service
+#	Then add lines:
+#	ExecStart=/usr/lib/bluetooth/bluetoothd -C
+#	ExecStartPost=/usr/bin/sdptool add SP
+# 5. sudo systemctl daemon-reload	
+# 6. sudo systemctl restart bluetooth.service
+# https://scribles.net/setting-up-bluetooth-serial-port-profile-on-raspberry-pi/
+
