7f5017d98e22a861e44e03a1aa00d758e8b46bd8
Denise Jaimes <twinlizzie@twinlizzie.home>
2021-04-15 16:04:39 +1200

    Quick updates for Arduino serial Bipedal robot control.

--- arduino/bipedalRobot_jetsonNano_serial/bipedalRobot_jetsonNano_serial.ino
+++ arduino/bipedalRobot_jetsonNano_serial/bipedalRobot_jetsonNano_serial.ino
@@ -5,6 +5,14 @@ int stepperState = 0;
 char gbxValues[] = "";
 char *gbxName = NULL;
 
+int L1_address = 0x13;
+int L2_address = 0x15;
+int L3_address = 0x17;
+
+int R1_address = 0x14;
+int R2_address = 0x16;
+int R3_address = 0x18;
+
 void setup(){
   Serial.begin(9600);
   Serial.setTimeout(10);
@@ -27,7 +35,33 @@ String getValue(String data, char separator, int index){
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 
-void moveGearbox(byte address, )
+void moveGearbox(int L1v){
+  if (L1v > 0){
+
+    int L1v_digit1 = 48 + ((L1v / 1000) % 10);
+    int L1v_digit2 = 48 + ((L1v / 100) % 10);
+    int L1v_digit3 = 48 + ((L1v / 10) % 10);
+    int L1v_digit4 = 48 + (L1v % 10);
+    
+    //Wire.beginTransmission(L1_address);
+    //Wire.write(48);
+    //Wire.write(48);
+    //Wire.write(48);
+    //Wire.write(48);
+    //Wire.write(48);
+    //Wire.endTransmission(); 
+
+    Serial.print("D1:");
+    Serial.print(L1v_digit1);
+    Serial.print(",D2:");
+    Serial.print(L1v_digit2);
+    Serial.print(",D3:");
+    Serial.print(L1v_digit3);
+    Serial.print(",D4:");
+    Serial.println(L1v_digit4);
+  }
+  //if (L2v > 0){}
+}
 
 void loop(){
   if (Serial.available() > 0){
@@ -52,5 +86,7 @@ void loop(){
     Serial.print(R2_gbx);
     Serial.print(", ");
     Serial.println(R3_gbx);
+
+    moveGearbox(L1_gbx);
   }
 }
